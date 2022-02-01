fcb32865aa95462628dd20ab3e0a8bbf90477e75
JideOgun <JideOgun@github.com>
2022-01-31 18:41:50 -0600

    hashed passwords

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -55,6 +55,7 @@ router.put('/:id', (req, res) => {
 // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
  // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
  User.update(req.body, {
+     individualHooks: true,
      where: {
          id: req.params.id
      }
@@ -62,7 +63,9 @@ router.put('/:id', (req, res) => {
  .then(dbUserData => {
      if(!dbUserData[0]) {
          res.status(404).json({ message: 'No user found with this id' });
+         return;
      }
+     res.json(dbUserData);
  })
  .catch(err => {
      console.log(err);
