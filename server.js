4b08928bf0255c267cecc821cd28d93991782946
JideOgun <JideOgun@github.com>
2022-02-08 01:05:43 -0600

    added tests and helper functions

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const app = express();
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const sess = {
   secret: 'Super secret secret',
